277b0e01587ce1bc977ac32871a340ae07a6d4b2
l.hao <l.hao.2012@qq.com>
2017-04-29 12:11:36 +0800

    admin

---
+++ app/Http/Controllers/admin/IndexController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+     */
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    public function index()
+    {
+
+    }
+}
